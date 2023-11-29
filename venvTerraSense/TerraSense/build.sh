089c0ab471b51bff80774a1892e6022a470bc215
Azzazeru <aaron.fuentesdioca@gmail.com>
2023-11-28 22:23:09 -0300

    Update build.sh

--- venvTerraSense/TerraSense/build.sh
+++ venvTerraSense/TerraSense/build.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 # poetry install
-# pip install -r requirements.txt
+# pip install -r requirements.txtpyut
+
+pip install --upgrade pip
 
 python manage.py collectstatic --no-input
 python manage.py migrate
